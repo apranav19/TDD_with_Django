52ebff67859eda89f8922fb476570c6ddf17d22e
Pranav Angara <apranav19@gmail.com>
2015-04-22 14:40:26 -0700

    Update functional test to reflect a basic user's workflow

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -12,7 +14,33 @@ class NewVisitorTest(unittest.TestCase):
         # Head over to the site
         self.browser.get('http://localhost:8000')
 
+        # Check if title is customized
         self.assertIn('To-Do lists', self.browser.title)
+
+        # Check if header is present
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # User enters a to-do item
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                input_box.get_attribute('placheholder'),
+                'Enter a to-do item'
+        )
+
+        # Buy an item
+        input_box.send_keys('Buy an item')
+
+        # When a user hits ENTER, the page must update
+        # to display a table of to-do items
+        input_box.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == '1: Buy an item' for row in rows)
+        )
+
         self.fail('Finish the test!')
 
 
